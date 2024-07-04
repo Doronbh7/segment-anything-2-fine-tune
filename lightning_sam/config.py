c7ba54de3e3292e00f303180c2693da6ce706112
Doronbh7 <Doronbh7@gmail.com>
2024-07-04 13:42:31 +0300

    updated gitignore

--- lightning_sam/config.py
+++ lightning_sam/config.py
@@ -16,7 +16,7 @@ config = {
     },
     "model": {
         "type": 'vit_h',
-        "checkpoint":"/home/user_218/SAM_Project/FineTune_lightning/lightning-sam/lightning_sam/out/training/epoch-000012-f10.96-ckpt.pth",##/home/user_218/SAM_Project/FineTune_lightning/lightning-sam/lightning_sam/sam_vit_h_4b8939.pth",
+        "checkpoint":"/home/user_218/SAM_Project/SAM-ARMBench/lightning_sam/sam_vit_h_4b8939.pth",##/home/user_218/SAM_Project/FineTune_lightning/lightning-sam/lightning_sam/sam_vit_h_4b8939.pth",
         "freeze": {
             "image_encoder": True,
             "prompt_encoder": True,
@@ -25,12 +25,12 @@ config = {
     },
     "dataset": {
         "train": {
-            "root_dir": "/home/user_218/SAM_Project/FineTune_lightning/lightning-sam/content/armbench-segmentation-0.1/same-object-transfer-set/images",
-            "annotation_file": "/home/user_218/SAM_Project/FineTune_lightning/lightning-sam/content/armbench-segmentation-0.1/same-object-transfer-set/train.json"
+            "root_dir": "/home/user_218/SAM_Project/SAM-ARMBench/content/armbench-segmentation-0.1/zoomed-out-tote-transfer-set/images",
+            "annotation_file": "/home/user_218/SAM_Project/SAM-ARMBench/content/armbench-segmentation-0.1/zoomed-out-tote-transfer-set/train.json"
         },
         "val": {
-                "root_dir": "/home/user_218/SAM_Project/FineTune_lightning/lightning-sam/content/armbench-segmentation-0.1/same-object-transfer-set/images",
-            "annotation_file": "/home/user_218/SAM_Project/FineTune_lightning/lightning-sam/content/armbench-segmentation-0.1/same-object-transfer-set/val.json"
+                "root_dir": "/home/user_218/SAM_Project/SAM-ARMBench/content/armbench-segmentation-0.1/zoomed-out-tote-transfer-set/images",
+            "annotation_file": "/home/user_218/SAM_Project/SAM-ARMBench/content/armbench-segmentation-0.1/zoomed-out-tote-transfer-set/val.json"
         }
     }
 }
